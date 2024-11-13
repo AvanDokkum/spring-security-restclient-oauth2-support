fda5ccc5a4b9c0bad9c9ed03a90a16b04b76bb1b
avandokkum <arjanvandokkum@gmail.com>
2024-11-13 19:50:42 +0100

    Initial commit - basic setup

---
+++ client-application/src/test/java/com/webcanis/client_application/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcanis.client_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
